7a0cf546dff400b94fc48d57fe8491eb855cbf75
Ruo Ting <ruoting@Ruo-MacBook-Air.attlocal.net>
2024-06-01 16:17:10 -0700

    disable the button inquire

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -6,13 +6,21 @@ function generateCode() {
     var code = ''; //initialize to null value
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 
-    //Generate character multiple times using a loop
-    for (i=1; i <= 8; i++) {
-    var char = Math.random()* str.length; //random selecy a character from the variable and then stroe in a new variable
-    code += str.charAt(char) //accumulate the generated character into a string of 8 characters   
-    }
-    return code;//return the final accumpated string when loop ends
+//Generate character multiple times using a loop
+for (i=1; i <= 8; i++) {
+var char = Math.random()* str.length; //random selecy a character from the variable and then stroe in a new variable
+code += str.charAt(char) //accumulate the generated character into a string of 8 characters   
+}
+return code;//return the final accumpated string when loop ends
 }
 
-    //Get HTML element to display
-    document.getElementById("codes").innerHTML = generateCode();
+//Get HTML element to display
+document.getElementById("codes").innerHTML = generateCode();
+
+//Disable Button 
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+//Activate function
+disableButton();
