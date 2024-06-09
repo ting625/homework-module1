8b3eaf0763166eb210a44d2e8837adceef2aa044
Ruo Ting <ruoting@Ruo-MacBook-Air.attlocal.net>
2024-06-09 10:48:52 -0700

    w5d3: add the enquire box by jQuery

--- js/activity.js
+++ js/activity.js
@@ -3,10 +3,36 @@
 
 $(document).ready(function(){
    $('td#cell').click(function(){
-    $(this).toggleClass('green');
+    $(this).toggleClass('tdhighlight');
    })
  });
 
- $(document).ready(function(){
-   $"td".click
+
+//User interaction with table cells
+
+  $(document).ready(function(){
+    $("td").click(function () { //user select a table data cell 
+      var content = $(this).text(); //get content of cell
+
+      if (content != "Not Available") {
+        $(this).toggleClass("tdhightlight");
+
+        if ($(this).hasClass("tdhighlight")) {
+            $('#displaySelected').css("visibility", "visible"); 
+            $('#displaySelected').css("margin-top", "2em"); 
+            $('#result').append("<p>"+content+ "</P>"); 
+
+        } else { //if selected cell don't have class
+          $('#result p:contains('+content+')').remove();;
+
+          if ($('#result').has('p').length == false) {
+              $('#displaySelected').css("visibility", "hidden");
+              $('#displaySelected').css("margin-top", "0");
+          
+          }
+        }
+      }
+      
+
+    });
  });
